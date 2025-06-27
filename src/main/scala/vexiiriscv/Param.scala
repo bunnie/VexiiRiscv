99fd8c460698afdc16dcd428aa37abf22cb6a789
Dolu1990 <charles.papon.90@gmail.com>
2025-06-27 19:01:44 +0200

    #61 add register based regfile support (--regfile-reg-based)

--- src/main/scala/vexiiriscv/Param.scala
+++ src/main/scala/vexiiriscv/Param.scala
@@ -98,6 +98,7 @@ class ParamSimple(){
   var dispatcherAt = 1
   var regFileSync = true
   var regFileDualPortRam = true
+  var regFileRegBasedRam = false
   var withGShare = false
   var withBtb = false
   var withRas = false
@@ -561,6 +562,7 @@ class ParamSimple(){
     opt[Unit]("regfile-sync") action { (v, c) => regFileSync = true }
     opt[Unit]("regfile-dual-ports") action { (v, c) => regFileDualPortRam = true }
     opt[Unit]("regfile-infer-ports") action { (v, c) => regFileDualPortRam = false }
+    opt[Unit]("regfile-reg-based") action { (v, c) => regFileRegBasedRam = true; regFileDualPortRam = false}
     opt[Int]("allow-bypass-from") action { (v, c) => allowBypassFrom = v }
     opt[Int]("performance-counters") unbounded() action { (v, c) => withPerformanceCounters = true; additionalPerformanceCounters = v }
     opt[Unit]("without-performance-scountovf") unbounded() action { (v, c) => withPerformanceScountovf = false }
@@ -760,6 +762,7 @@ class ParamSimple(){
       preferedWritePortForInit = "lane0",
       syncRead = regFileSync,
       dualPortRam = regFileDualPortRam,
+      regBasedRam = regFileRegBasedRam,
       maskReadDuringWrite = false
     )
 
@@ -975,6 +978,7 @@ class ParamSimple(){
         preferedWritePortForInit = "lane0",
         syncRead = regFileSync,
         dualPortRam = regFileDualPortRam,
+        regBasedRam = regFileRegBasedRam,
         maskReadDuringWrite = false
       )
       plugins += new WriteBackPlugin(lane0, FloatRegFile, writeAt = 9, allowBypassFrom = allowBypassFrom.max(2)) //Max 2 to save area on not so important instructions
